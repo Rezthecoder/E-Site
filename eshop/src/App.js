b395bcc0e634538da960096e44db63f386934f9f
Rezthecoder <18te427@ditu.jp>
2023-12-10 13:07:41 +0900

    added Header.js and Header.css

--- eshop/src/App.js
+++ eshop/src/App.js
@@ -1,9 +1,13 @@
-import './App.css';
-
+import "./App.css";
+import "./Header.css"
+// import ShoppingBasketIcon from '@mui/icons-material/ShoppingBasket';
+// import StorefrontIcon from '@mui/icons-material/Storefront';
+// import SearchIcon from '@mui/icons-material/Search';
+import Header from "./Header";
 function App() {
   return (
     <div className="App">
-      
+     <Header/>
     </div>
   );
 }
