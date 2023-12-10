3942964390a8f1b84d8c4d4056dc4a747839ce05
Rezthecoder <18te427@ditu.jp>
2023-12-10 22:43:51 +0900

    modified App.css App.js Header.css Header.js index.css index.js Home.css Home.js Product.css Product.js

--- eshop/src/App.js
+++ eshop/src/App.js
@@ -1,13 +1,13 @@
-import "./App.css";
-import "./Header.css"
-// import ShoppingBasketIcon from '@mui/icons-material/ShoppingBasket';
-// import StorefrontIcon from '@mui/icons-material/Storefront';
-// import SearchIcon from '@mui/icons-material/Search';
+import './App.css';
+import Home from "./Home";
 import Header from "./Header";
+
 function App() {
   return (
     <div className="App">
-     <Header/>
+      <Header />
+      <Home />
+
     </div>
   );
 }
--- eshop/src/Header.js
+++ eshop/src/Header.js
@@ -1,36 +1,40 @@
-import React from 'react'
-import ShoppingBasketIcon from '@mui/icons-material/ShoppingBasket';
-import StorefrontIcon from '@mui/icons-material/Storefront';
-import SearchIcon from '@mui/icons-material/Search';
-import "./Header.css"
+import React from "react";
+import "./Header.css";
+import ShoppingBasketIcon from "@mui/icons-material/ShoppingBasket";
+import StorefrontIcon from "@mui/icons-material/Storefront";
+import SearchIcon from "@mui/icons-material/Search";
 
 function Header() {
   return (
-    <div className='header'>
- <div className="header_logo">
-        <StorefrontIcon className="header_logoImage" fontSize="large"/>
-        <h2 className="header_logoTitle">E-shop</h2>
+    <div className="header">
+      <div className="header__logo">
+        <StorefrontIcon className="header__logoImage" fontSize="large" />
+        <h2 className="header__logoTitle">eShop</h2>
       </div>
-      <div className="header_search">
-        <input type="text" className="header_searchInput" />
-        <SearchIcon className="header_searchIcon" fontSize="large"/>
+
+      <div className="header__search">
+        <input type="text" className="header__searchInput" />
+        <SearchIcon className="header__searchIcon" />
       </div>
-      <div className="header_nav">
-        <div className="nav_item">
-          <span className="nav_itemLineOne">Hello Guest</span>
-          <span className="nav_itemLineTwo">Sign in</span>
+
+      <div className="header__nav">
+        <div className="nav__item">
+          <span className="nav__itemLineOne">Hello Guest</span>
+          <span className="nav__itemLineTwo">Sign In</span>
         </div>
-        <div className="nav_item">
-          <span className="nav_itemLineOne">Your</span>
-          <span className="nav_itemLineTwo">SHop</span>
+
+        <div className="nav__item">
+          <span className="nav__itemLineOne">Your</span>
+          <span className="nav__itemLineTwo">Shop</span>
         </div>
-        <div className="nav_item">
-          <ShoppingBasketIcon className="itemBasket" />
-          <span className="nav_itemLineTwo nav_basketCount">0</span>
+
+        <div className="nav__itemBasket">
+          <ShoppingBasketIcon />
+          <span className="nav__itemLineTwo nav__basketCount">0</span>
         </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default Header
+export default Header;
