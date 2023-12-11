33c6450f31932dfb581ac320f0318c8ce2646b25
Rezthecoder <18te427@ditu.jp>
2023-12-11 14:29:37 +0900

    Revert "updated"
    This reverts commit 866f6fd940d6910b94ed03c83ed74c9fa0e02b9c.

--- eshop/src/Header.js
+++ eshop/src/Header.js
@@ -8,12 +8,10 @@ import { Link } from "react-router-dom";
 function Header() {
   return (
     <div className="header">
-      <Link to="/" style={{ textdecoration: "none" }}>
-        <div className="header__logo">
-          <StorefrontIcon className="header__logoImage" fontSize="large" />
-          <h2 className="header__logoTitle">eShop</h2>
-        </div>
-      </Link>
+      <div className="header__logo">
+        <StorefrontIcon className="header__logoImage" fontSize="large" />
+        <h2 className="header__logoTitle">eShop</h2>
+      </div>
 
       <div className="header__search">
         <input type="text" className="header__searchInput" />
@@ -31,12 +29,13 @@ function Header() {
           <span className="nav__itemLineTwo">Shop</span>
         </div>
 
-        <Link to="/checkout" style={{ textdecoration: "none" }}>
-          <div className="nav__itemBasket">
-            <ShoppingBasketIcon />
-            <span className="nav__itemLineTwo nav__basketCount">0</span>
-          </div>
-        </Link>
+<Link to='/checkout'>
+<div className="nav__itemBasket">
+          <ShoppingBasketIcon />
+          <span className="nav__itemLineTwo nav__basketCount">0</span>
+        </div>
+</Link>
+       
       </div>
     </div>
   );
